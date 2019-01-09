6a54bbb4e15ca527815f1d8a120cf39c7f59167b
Nejc <dev.neetsu@gmail.com>
2019-01-10 00:42:27 +0100

    update the Fille access rights. Check on them at first run

--- app/src/main/java/com/di/ne/diplomskafi/Aplikacija.java
+++ app/src/main/java/com/di/ne/diplomskafi/Aplikacija.java
@@ -1,10 +1,14 @@
 package com.di.ne.diplomskafi;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -50,6 +54,15 @@ public class Aplikacija extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         List<Nastavitve> nast = db.readNastavitve();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+             ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+
+
         if(nast.size()!=0){
             File = nast.get(0).getFilePath();
         }else {
